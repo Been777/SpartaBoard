d07b8889965c0d3eb515c7711acf753726dc493e
MonGroy <mongtualmr@gmail.com>
2024-06-09 15:49:34 +0900

    Comment: 주석 삭제

--- src/main/java/org/example/spartaboard/controller/ProfileController.java
+++ src/main/java/org/example/spartaboard/controller/ProfileController.java
@@ -8,6 +8,7 @@ import org.example.spartaboard.dto.ProfileResponseDto;
 import org.example.spartaboard.service.ProfileService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -36,7 +37,4 @@ public class ProfileController {
         return profileService.updateProfile(modifyRequestDto, userId);
     }
 
-//    private boolean authorizePassword(String newPassword) {
-//        return false;
-//    }
 }
