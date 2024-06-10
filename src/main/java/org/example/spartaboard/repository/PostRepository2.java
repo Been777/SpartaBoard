e91aa972c64f99f3e0bb27b02ef2362088e368d9
MonGroy <mongtualmr@gmail.com>
2024-06-10 19:37:06 +0900

    Refactor: 사용하지 않는 클래스 삭제

--- src/main/java/org/example/spartaboard/repository/PostRepository2.java
+++
@@ -1,8 +0,0 @@
-package org.example.spartaboard.repository;
-
-import org.example.spartaboard.entity.Post2;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository2 extends JpaRepository<Post2, Long> {
-
-}
